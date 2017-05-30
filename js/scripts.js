f99a8ee5277144f6dea4327edf64234760760cbd
Lukasz Sliczner <lukaszsliczner@MacBook-Pro.local>
2017-05-30 21:15:04 +0000

    tab correction in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
-var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'],
+var	femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'],
     maleNames = ['Piotrek', 'Marek', 'Jarek', 'Arek'],
     allNames = femaleNames.concat(maleNames),
     newName = 'Marian';
 
 if(allNames.indexOf(newName) === -1) {
-  allNames.push(newName);
+	allNames.push(newName);
 } else {
-  console.log('Sorry, this name already exist in the array :-(');
+	console.log('Sorry, this name already exist in the array :-(');
 }
